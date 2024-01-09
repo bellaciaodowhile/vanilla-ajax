2bf07353975f6c3b37e4fbc4b6baa1467571253f
bellaciaodowhile <125389712+bellaciaodowhile@users.noreply.github.com>
2024-01-08 20:31:56 -0400

    Last version

--- ajax.js
+++ ajax.js
@@ -1,21 +1,32 @@
-function AJAX({ url, data = [], success, error }) {
+function AJAXGJ8({
+    url,
+    data = [],
+    success,
+    error
+}) {
     let req = (window.XMLHttpRequest) ? new XMLHttpRequest() : ActiveXObject('Microsoft.XMLHTTP');
     req.open("POST", BASE_URL + url, true);
+
     function dataFn() {
         let insideData = '';
-        if (data.length > 0) {
-            data.map((x) => {
-                Object.keys(x).map((field, indexField) => {
-                    if (indexField == 0) {
-                        insideData += Object.keys(x)[indexField] + '=' + (Array.isArray(Object.values(x)[indexField]) ? JSON.stringify(Object.values(x)[indexField]) : Object.values(x)[indexField]) 
-                    } else {
-                        insideData += '&' + Object.keys(x)[indexField] + '=' + (Array.isArray(Object.values(x)[indexField]) ? JSON.stringify(Object.values(x)[indexField]) : Object.values(x)[indexField]) 
-                    }
-                })
-            });
+        data.map((x) => {
+            Object.keys(x).map((field, indexField) => {
+                if (indexField == 0) {
+                    insideData += Object.keys(x)[indexField] + '=' + (Array.isArray(Object.values(x)[indexField]) ? JSON.stringify(Object.values(x)[indexField]) : Object.values(x)[indexField].toString().trim())
+                } else {
+                    insideData += '&' + Object.keys(x)[indexField] + '=' + (Array.isArray(Object.values(x)[indexField]) ? JSON.stringify(Object.values(x)[indexField]) : (typeof Object.values(x)[indexField] == 'string' ? Object.values(x)[indexField].trim() : Object.values(x)[indexField]))
+                }
+            })
+            if (data.length > 1) {
+                insideData += '__';
+            }
+        });
+        if (data.length > 1) {
+            insideData = 'arrData=' + JSON.stringify(data);
         }
         return insideData;
     }
+    // console.log(dataFn())
     req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     req.send(dataFn())
     req.onreadystatechange = (e) => {
